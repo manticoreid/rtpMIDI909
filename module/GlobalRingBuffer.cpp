e991408d6330ba188bc0763ac4fc623971f86db5
Budi Prakosa <iyok@deadmediafm.org>
2018-06-24 19:55:12 +0700

    initial module from microbemodular

---
+++ module/GlobalRingBuffer.cpp
@@ -0,0 +1,3 @@
+#include "Arduino.h"
+
+uint16_t RING_BUFFER[4096];
---
+++ module/GlobalRingBuffer.h
@@ -0,0 +1,8 @@
+#include "Arduino.h"
+
+#ifndef RingBuffer_h
+#define RingBuffer_h
+
+extern uint16_t RING_BUFFER[4096];
+
+#endif
